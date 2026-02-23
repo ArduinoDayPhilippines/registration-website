0ab8988e459bf76310dec839ad4205bf6ec89648
ezekielgbustamante@iskolarngbayan.pup.edu.ph <ezekielgbustamante@iskolarngbayan.pup.edu.ph>
2026-02-23 16:10:08 +0800

    refactor: modularize data types for registrant actions and guest management

--- frontend/src/components/manage-event/GuestAnswersModal.tsx
+++ frontend/src/components/manage-event/GuestAnswersModal.tsx
@@ -57,9 +57,7 @@ export function GuestAnswersModal({ guest, event, onClose }: GuestAnswersModalPr
 
         {/* Content */}
         <div className="relative flex-1 overflow-y-auto custom-scrollbar p-6 md:p-8">
-          {guest.form_answers &&
-          typeof guest.form_answers === "object" &&
-          Object.keys(guest.form_answers).length > 0 ? (
+          {Object.keys(guest.form_answers).length > 0 ? (
             <div className="space-y-4">
               {Object.entries(guest.form_answers).map(
                 ([answerKey, answer], index) => {
